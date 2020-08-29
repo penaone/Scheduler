dde7e37582064af258eaebc61644917f0168d7f6
penaone <penaone@gmail.com>
2020-08-29 13:19:49 -0700

    Final update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Scheduler
+
+This project required the building of a work schedule app.
+By using moment.js I was able to use accurate dates for the app.
+With jquery and bootstrap and awesome I was able leverage there libraries to make the webpage look good and run easily.
+
+The challenge requires the data entered to be stored in local storage so that it remains on the page when refreshed.
+
+This was my biggest challenge. It took me several hours and help from a tutor find what piece of code I was missing.
+
+![](/images/schedulerscreenshot.PNG)
+
+repo address :https://github.com/penaone/Scheduler
+
+https://penaone.github.io/Scheduler/
+
--- assets/schedapp.js
+++ assets/schedapp.js
@@ -1,6 +1,7 @@
 var $currentDay = $("#currentDay");
 var $timeBlocks = $(".time-block");
 var $scheduleArea = $(".schedule");
+
 var toDoItems = [];
 // format date and time properties
 
@@ -9,84 +10,100 @@ var currentHour = moment().format("H");
 
 
 function beginSchedule(){
+    
+
+// var storage = localStorage.getItem
     $timeBlocks.each(function(){
         var $thisBlock = $(this);
+        
         var thisBlockTime = parseInt($thisBlock.attr("time-sig"));
-
+        
         var todoObj = {
+
             time: thisBlockTime,
+
             text: "",
         }
+
         toDoItems.push(todoObj);
     });
 
 //save array to local storage
 localStorage.setItem("todos", JSON.stringify(toDoItems));
-console.log(toDoItems);
+
 
 }
+
 function prepareTimeBlocks(){
     $timeBlocks.each(function(){
         var $thisBlock = $(this);
         var thisBlockTime = parseInt($thisBlock.attr("time-sig"));
-  
-
 // show current date
 
     if (thisBlockTime == currentHour) {
-
         $thisBlock.addClass("present").removeClass("past future");
     }
-
     if (thisBlockTime < currentHour) {
         $thisBlock.addClass("past").removeClass("present future");
     }
     if (thisBlockTime > currentHour) {
         $thisBlock.addClass("future").removeClass("past present");
     }
-
 });
 }
 
-
 function runSchedule() {
+    
 
     toDoItems = localStorage.getItem("todos");
     toDoItems = JSON.parse(toDoItems);
+    
+
+
+
     for (var x = 0; x < toDoItems.length; x++){
         var itemTime = toDoItems[x].time;
         var itemText = toDoItems[x].text;
         
         $("[time-sig=" + itemTime + "]").children("textarea").val(itemText);
     }
-    
+
+  
 }
 
 function saveHandler() {
+    
         var $thisBlock = $(this).parent();
+
         var timeToUpdate = $(this).parent().attr("time-sig");
         var itemToAdd = (($(this).parent()).children("textarea")).val();
 
         for (var i = 0; i < toDoItems.length; i++) {
             if (toDoItems[i].time == timeToUpdate) {
+
                 toDoItems[i].text = itemToAdd;
             }
         }
-        //send to local storage
         localStorage.setItem("todos", JSON.stringify(toDoItems));
         runSchedule();
-        console.log(toDoItems);
+        
 
     }
 $(document).ready(function(){
+
+
     prepareTimeBlocks();
+    runSchedule();
+
     if(!localStorage.getItem("todos")){
-        beginSchedule();
+
+    beginSchedule();
+    }
 
         // display today's date
         $currentDay.text(todaysDate);
         $scheduleArea.on("click", "button", saveHandler);
     
 
-    }
+    
 });
