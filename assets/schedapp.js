1bd4de5e90839523dace844b8cbea595847473f0
penaone <penaone@gmail.com>
2020-08-29 14:20:27 -0700

    cleaned up formatting

--- assets/schedapp.js
+++ assets/schedapp.js
@@ -9,15 +9,15 @@ var todaysDate = moment().format("dddd, MMMM Do");
 var currentHour = moment().format("H");
 
 
-function beginSchedule(){
-    
+function beginSchedule() {
 
-// var storage = localStorage.getItem
-    $timeBlocks.each(function(){
+
+    // var storage = localStorage.getItem
+    $timeBlocks.each(function () {
         var $thisBlock = $(this);
-        
+
         var thisBlockTime = parseInt($thisBlock.attr("time-sig"));
-        
+
         var todoObj = {
 
             time: thisBlockTime,
@@ -28,82 +28,82 @@ function beginSchedule(){
         toDoItems.push(todoObj);
     });
 
-//save array to local storage
-localStorage.setItem("todos", JSON.stringify(toDoItems));
+    //save array to local storage
+    localStorage.setItem("todos", JSON.stringify(toDoItems));
 
 
 }
 
-function prepareTimeBlocks(){
-    $timeBlocks.each(function(){
+function prepareTimeBlocks() {
+    $timeBlocks.each(function () {
         var $thisBlock = $(this);
         var thisBlockTime = parseInt($thisBlock.attr("time-sig"));
-// show current date
+        // show current date
 
-    if (thisBlockTime == currentHour) {
-        $thisBlock.addClass("present").removeClass("past future");
-    }
-    if (thisBlockTime < currentHour) {
-        $thisBlock.addClass("past").removeClass("present future");
-    }
-    if (thisBlockTime > currentHour) {
-        $thisBlock.addClass("future").removeClass("past present");
-    }
-});
+        if (thisBlockTime == currentHour) {
+            $thisBlock.addClass("present").removeClass("past future");
+        }
+        if (thisBlockTime < currentHour) {
+            $thisBlock.addClass("past").removeClass("present future");
+        }
+        if (thisBlockTime > currentHour) {
+            $thisBlock.addClass("future").removeClass("past present");
+        }
+    });
 }
 
 function runSchedule() {
-    
+
 
     toDoItems = localStorage.getItem("todos");
     toDoItems = JSON.parse(toDoItems);
-    
 
 
 
-    for (var x = 0; x < toDoItems.length; x++){
+
+    for (var x = 0; x < toDoItems.length; x++) {
         var itemTime = toDoItems[x].time;
         var itemText = toDoItems[x].text;
-        
+
         $("[time-sig=" + itemTime + "]").children("textarea").val(itemText);
     }
 
-  
+
 }
 
 function saveHandler() {
-    
-        var $thisBlock = $(this).parent();
 
-        var timeToUpdate = $(this).parent().attr("time-sig");
-        var itemToAdd = (($(this).parent()).children("textarea")).val();
+    var $thisBlock = $(this).parent();
 
-        for (var i = 0; i < toDoItems.length; i++) {
-            if (toDoItems[i].time == timeToUpdate) {
+    var timeToUpdate = $(this).parent().attr("time-sig");
+    var itemToAdd = (($(this).parent()).children("textarea")).val();
 
-                toDoItems[i].text = itemToAdd;
-            }
+    for (var i = 0; i < toDoItems.length; i++) {
+        if (toDoItems[i].time == timeToUpdate) {
+
+            toDoItems[i].text = itemToAdd;
         }
-        localStorage.setItem("todos", JSON.stringify(toDoItems));
-        runSchedule();
-        
-
     }
-$(document).ready(function(){
+    localStorage.setItem("todos", JSON.stringify(toDoItems));
+    runSchedule();
+
+
+}
+$(document).ready(function () {
 
 
     prepareTimeBlocks();
     runSchedule();
 
-    if(!localStorage.getItem("todos")){
+    if (!localStorage.getItem("todos")) {
 
-    beginSchedule();
+        beginSchedule();
     }
 
-        // display today's date
-        $currentDay.text(todaysDate);
-        $scheduleArea.on("click", "button", saveHandler);
-    
+    // display today's date
+    $currentDay.text(todaysDate);
+    $scheduleArea.on("click", "button", saveHandler);
 
-    
+
+
 });
